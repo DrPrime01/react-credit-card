239da575b53a0a5dbf37ca9dcd9025f54a00229f
Gabriel Shoyombo <gabrielshoyombo2002@gmail.com>
2022-08-12 17:41:00 +0100

    A credit card static website made with React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function CreditCard({cardinfo}) {
+  return (
+    <div className='credit-card'>
+      <Bank bank={cardinfo.bank} />
+      <CardNumber cardNumber={cardinfo.cardNumber} />
+      <ExpiryDate date={cardinfo.expiryDate} />
+      <CardHolder name={cardinfo.cardHolder} />
+    </div>
+  )
+}
+
+function Bank({bank}) {
+  return (
+    <div className='bank-name'>{bank}</div>
+  )
+}
+
+function CardNumber({cardNumber}) {
+  return (
+    <div className='card-number'>
+      <span className='number'>{cardNumber}</span>
+      <span className='num'>0123</span>
+    </div>
+  )
+}
+
+function ExpiryDate({date}) {
+  return (
+    <div className='expiry-date'>
+      <div>Valid Thru</div>
+      <span className='date'>{date}</span>
+    </div>
+  )
+}
+
+function CardHolder({name}) {
+  return (
+    <div className='card-holder-name'>
+      {name}
+    </div>
+  )
+}
+
+const cardDetails = {
+  bank: "Big Bank, Inc.",
+  cardNumber: "0123 4567 8910 1112",
+  expiryDate: "08/12",
+  cardHolder: "John Doe"
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<CreditCard cardinfo={cardDetails} />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
